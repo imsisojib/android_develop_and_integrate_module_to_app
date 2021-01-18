dd38e15e4297c23cd5f585e99dbc2b45ab149e52
imsisojib <sirajulislam1123@gmail.com>
2021-01-18 13:10:07 +0600

    moduleIntegratedApp -> problem solved. implementation (path: ":app-debug2") change to implementation project(":app-debug2")

--- app/src/main/java/com/example/moduleintegratedapp/MainXActivity.java
+++ app/src/main/java/com/example/moduleintegratedapp/MainXActivity.java
@@ -5,15 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.demomodule.MainActivity;
+
+public class MainXActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_mainx);
 
         findViewById(R.id.button_open_activity_from_module).setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, com.example.demomodule.MainActivity.class);
+            Intent intent = new Intent(MainXActivity.this, MainActivity.class);
             startActivity(intent);
         });
 
